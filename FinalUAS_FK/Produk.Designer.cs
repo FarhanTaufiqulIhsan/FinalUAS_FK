6f7190760e17d4bc18462e5293be5454964d07eb
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-07-10 23:20:04 +0700

    Menambahkan method untuk kembali ke halaman utama pada button Back di form Produk

--- FinalUAS_FK/Produk.Designer.cs
+++ FinalUAS_FK/Produk.Designer.cs
@@ -167,6 +167,7 @@
             this.btnBack.TabIndex = 13;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // Produk
             // 
