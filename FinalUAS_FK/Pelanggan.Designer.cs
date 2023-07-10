71ad78c66ec1d0c4b51be2a60b8dcb29d9846e7b
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-07-10 23:54:54 +0700

    Menambahkan method untuk kembali ke menu utama pada button Back di form Pelanggan

--- FinalUAS_FK/Pelanggan.Designer.cs
+++ FinalUAS_FK/Pelanggan.Designer.cs
@@ -185,6 +185,7 @@
             this.btnBack.TabIndex = 16;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // Pelanggan
             // 
--- FinalUAS_FK/Pelanggan.cs
+++ FinalUAS_FK/Pelanggan.cs
@@ -118,5 +118,12 @@ namespace FinalUAS_FK
                 refreshform();
             }
         }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+        }
     }
 }
