574c312ed9a9856514a7b86576ad9f8d906a03d2
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-07-10 22:55:27 +0700

    Menambahkan method untuk kembali ke menu utama pada button Back di form Penjual

--- FinalUAS_FK/Penjual.Designer.cs
+++ FinalUAS_FK/Penjual.Designer.cs
@@ -185,6 +185,7 @@
             this.btnBack.TabIndex = 15;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // Penjual
             // 
