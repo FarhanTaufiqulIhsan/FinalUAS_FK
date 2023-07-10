4283bc37276d75693e766265909eaf2f9921b7c6
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-07-10 22:57:47 +0700

    Membuat method koneksi pada form Produk

--- FinalUAS_FK/Produk.cs
+++ FinalUAS_FK/Produk.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace FinalUAS_FK
 {
     public partial class Produk : Form
     {
+        private string stringConnection = "data source=LAPTOP-P9JKEJMQ\\FARHANSQL;" + "database=Pemesanan_Baju_UAS;User ID=sa;password=Laserin45@";
+        private SqlConnection koneksi;
         public Produk()
         {
             InitializeComponent();
