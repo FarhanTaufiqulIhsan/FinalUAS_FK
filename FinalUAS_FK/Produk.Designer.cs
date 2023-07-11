8eaba3a20aa1cb44f51d48f2ccd2430948fc6f10
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-07-11 12:56:09 +0700

    Menambahkan Button Jenis Produk diForm Produk

--- FinalUAS_FK/Produk.Designer.cs
+++ FinalUAS_FK/Produk.Designer.cs
@@ -42,6 +42,7 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
+            this.btnJp = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -169,11 +170,21 @@
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // btnJp
+            // 
+            this.btnJp.Location = new System.Drawing.Point(521, 385);
+            this.btnJp.Name = "btnJp";
+            this.btnJp.Size = new System.Drawing.Size(107, 23);
+            this.btnJp.TabIndex = 14;
+            this.btnJp.Text = "Jenis Produk";
+            this.btnJp.UseVisualStyleBackColor = true;
+            // 
             // Produk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnJp);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSave);
@@ -213,5 +224,6 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnJp;
     }
 }
