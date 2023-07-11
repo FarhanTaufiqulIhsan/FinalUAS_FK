1ec70cad6bd3fff6f7571f2f398cf2eeb832b729
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-07-12 00:16:55 +0700

    Menambahkan button back pada form Transaksi PP

--- FinalUAS_FK/TransaksiPP.Designer.cs
+++ FinalUAS_FK/TransaksiPP.Designer.cs
@@ -193,6 +193,7 @@
             this.btnBack.TabIndex = 16;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // TransaksiPP
             // 
--- FinalUAS_FK/TransaksiPP.cs
+++ FinalUAS_FK/TransaksiPP.cs
@@ -16,5 +16,12 @@ namespace FinalUAS_FK
         {
             InitializeComponent();
         }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Pelanggan p = new Pelanggan();
+            p.Show();
+            this.Hide();
+        }
     }
 }
