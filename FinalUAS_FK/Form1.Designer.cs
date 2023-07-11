1792fec408098195ee14e1248b2668e3dc552f7d
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-07-12 01:51:15 +0700

    Mendesain Form1

--- FinalUAS_FK/Form1.Designer.cs
+++ FinalUAS_FK/Form1.Designer.cs
@@ -32,13 +32,12 @@
             this.btnPenjual = new System.Windows.Forms.Button();
             this.btnProduk = new System.Windows.Forms.Button();
             this.btnPelanggan = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSuplier
             // 
-            this.btnSuplier.Location = new System.Drawing.Point(71, 197);
+            this.btnSuplier.Location = new System.Drawing.Point(65, 197);
             this.btnSuplier.Name = "btnSuplier";
             this.btnSuplier.Size = new System.Drawing.Size(113, 59);
             this.btnSuplier.TabIndex = 0;
@@ -48,7 +47,7 @@
             // 
             // btnPenjual
             // 
-            this.btnPenjual.Location = new System.Drawing.Point(226, 197);
+            this.btnPenjual.Location = new System.Drawing.Point(235, 197);
             this.btnPenjual.Name = "btnPenjual";
             this.btnPenjual.Size = new System.Drawing.Size(113, 59);
             this.btnPenjual.TabIndex = 1;
@@ -58,7 +57,7 @@
             // 
             // btnProduk
             // 
-            this.btnProduk.Location = new System.Drawing.Point(377, 197);
+            this.btnProduk.Location = new System.Drawing.Point(420, 197);
             this.btnProduk.Name = "btnProduk";
             this.btnProduk.Size = new System.Drawing.Size(113, 59);
             this.btnProduk.TabIndex = 2;
@@ -68,7 +67,7 @@
             // 
             // btnPelanggan
             // 
-            this.btnPelanggan.Location = new System.Drawing.Point(529, 197);
+            this.btnPelanggan.Location = new System.Drawing.Point(595, 197);
             this.btnPelanggan.Name = "btnPelanggan";
             this.btnPelanggan.Size = new System.Drawing.Size(113, 59);
             this.btnPelanggan.TabIndex = 3;
@@ -76,22 +75,13 @@
             this.btnPelanggan.UseVisualStyleBackColor = true;
             this.btnPelanggan.Click += new System.EventHandler(this.btnPelanggan_Click);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(691, 233);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(173, 33);
+            this.label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(484, 69);
+            this.label1.Size = new System.Drawing.Size(441, 75);
             this.label1.TabIndex = 5;
             this.label1.Text = "Pemesanan Baju";
             // 
@@ -101,7 +91,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.btnPelanggan);
             this.Controls.Add(this.btnProduk);
             this.Controls.Add(this.btnPenjual);
@@ -120,7 +109,6 @@
         private System.Windows.Forms.Button btnPenjual;
         private System.Windows.Forms.Button btnProduk;
         private System.Windows.Forms.Button btnPelanggan;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label1;
     }
 }
